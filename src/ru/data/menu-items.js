2f5665b61cbf790fdd87b7ed9eeb9c7b4e67adae
Albert Steppi <albert.steppi@gmail.com>
2024-06-21 14:42:05 -0400

    New translations menu-items.js (Russian)

---
+++ src/ru/data/menu-items.js
@@ -0,0 +1,30 @@
+export const menuItems = [
+    {
+        label: 'Features',
+        href: '/#features'
+    },
+    {
+        label: 'Try',
+        href: '/#repl'
+    },
+    {
+        label: 'Ecosystem',
+        href: '/#ecosystem'
+    },
+    {
+        label: 'Sponsors',
+        href: '/#sponsors'
+    },
+    {
+        label: 'Donate',
+        href: '/#donate'
+    },
+    {
+        label: 'Blog',
+        href: '/blog'
+    },
+    {
+        label: 'Documentation',
+        href: 'https://docs.xarray.dev'
+    }
+];
