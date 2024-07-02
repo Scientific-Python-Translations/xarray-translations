cd29c45480e6f8445c078bd1e1f4a57ccba5efca
steppi <albert.steppi@gmail.com>
2024-07-02 13:58:53 -0400

    Update macro wrapping script, process nested elements

--- add_lingui_macros_components.js
+++ add_lingui_macros_components.js
@@ -13,14 +13,10 @@ module.exports = function(fileInfo, { jscodeshift: j }) {
         j.literal('@lingui/macro')
       )
     );
-    console.log('Added import for @lingui/macro');
   }
 
-  // Function to wrap text nodes in the `t` macro using template literals
-  const wrapTextNode = (node) => j.taggedTemplateExpression(
-    j.identifier('t'),
-    j.templateLiteral([j.templateElement({ raw: node.value, cooked: node.value }, true)], [])
-  );
+  // Function to wrap text nodes in the `t` macro
+  const wrapTextNode = (node) => j.callExpression(j.identifier('t'), [node]);
 
   // Process <Text> and <Heading> nodes
   root.find(j.JSXElement)
@@ -30,36 +26,30 @@ module.exports = function(fileInfo, { jscodeshift: j }) {
     })
     .forEach(path => {
       let modified = false;
-      path.node.children = path.node.children.map(child => {
-        if (child.type === 'JSXText') {
-          const trimmedValue = child.value.trim();
-          if (trimmedValue) {
-            modified = true;
-            return j.jsxExpressionContainer(wrapTextNode(j.stringLiteral(trimmedValue)));
-          }
-        } else if (child.type === 'JSXExpressionContainer') {
-          const expression = child.expression;
-          if (expression.type === 'Literal' && typeof expression.value === 'string') {
-            const trimmedValue = expression.value.trim();
+
+      const processChildren = (children) => {
+        return children.map(child => {
+          if (child.type === 'JSXText') {
+            const trimmedValue = child.value.trim();
             if (trimmedValue) {
               modified = true;
               return j.jsxExpressionContainer(wrapTextNode(j.stringLiteral(trimmedValue)));
             }
-          } else if (expression.type === 'CallExpression' && expression.arguments.length > 0) {
-            expression.arguments = expression.arguments.map(arg => {
-              if (arg.type === 'Literal' && typeof arg.value === 'string') {
-                const trimmedValue = arg.value.trim();
-                if (trimmedValue) {
-                  modified = true;
-                  return wrapTextNode(j.stringLiteral(trimmedValue));
-                }
-              }
-              return arg;
-            });
+          } else if (child.type === 'JSXExpressionContainer' && child.expression.type === 'Literal') {
+            const trimmedValue = child.expression.value.trim();
+            if (trimmedValue) {
+              modified = true;
+              return j.jsxExpressionContainer(wrapTextNode(j.stringLiteral(trimmedValue)));
+            }
+          } else if (child.type === 'JSXElement') {
+            child.children = processChildren(child.children);
           }
-        }
-        return child;
-      });
+          return child;
+        });
+      };
+
+      path.node.children = processChildren(path.node.children);
+
       if (modified) {
         console.log(`Modified tag: ${path.node.openingElement.name.name}`);
       }
