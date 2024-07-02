6dcdcbeeafe1af7b6b090f94e408084f1bd8285e
steppi <albert.steppi@gmail.com>
2024-07-02 12:15:53 -0400

    Delete everything which needs deleting

--- src/en/components/mdx/image.jsx
+++
@@ -1,9 +0,0 @@
-import { Image as ChakraImage } from '@chakra-ui/react'
-
-// Not functional: https://github.com/chakra-ui/chakra-ui/discussions/2475
-export const Image = (props) => {
-  const { src, alt, ...rest } = props
-  return (
-    <ChakraImage src={src} alt={alt} {...rest} loading='lazy'></ChakraImage>
-  )
-}
--- src/en/lib/sanitize-html.js
+++
@@ -1,6 +0,0 @@
-// utils/sanitizeHtml.js
-import createDOMPurify from 'isomorphic-dompurify';
-
-export function sanitizeHTML(html) {
-  return createDOMPurify.sanitize(html);
-}
